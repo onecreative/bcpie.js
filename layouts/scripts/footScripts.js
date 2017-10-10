0dce4642eb752d2c19803b9e47e1846a0e90b3ec
Adam Cook <adam@onecreative.pro>
2017-10-10 00:44:33 -0500

    layouts

---
+++ layouts/scripts/footScripts.js
@@ -0,0 +1,6 @@
+/*
+ *
+ * footScripts.js is where all library and plugin scripts go. Don't put your invocations and custom scripts in here. That's what readyScripts.js is for.
+ *
+ */
+
---
+++ layouts/scripts/headScripts.js
@@ -0,0 +1,6 @@
+/*
+ *
+ * headScripts.js is where you put scripts that have to be in the head. There really shouldn't be that many to put here. 99.9% of the time, footScripts.js is a better place.
+ *
+ */
+
